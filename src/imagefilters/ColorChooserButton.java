cd9fc96def2247bf024b2cb4b112dbf4ed26a4b0
psoder3 <paulsoderquist3@gmail.com>
2018-05-04 18:43:04 -0600

    Allowed in-memory save of mask colors

--- src/imagefilters/ColorChooserButton.java
+++ src/imagefilters/ColorChooserButton.java
@@ -1,82 +1,89 @@
 package imagefilters;
 
 import java.awt.Color;
-    import java.awt.Graphics2D;
-    import java.awt.event.ActionEvent;
-    import java.awt.event.ActionListener;
-    import java.awt.image.BufferedImage;
-    import java.util.ArrayList;
-    import java.util.List;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
 
-    import javax.swing.ImageIcon;
-    import javax.swing.JButton;
-    import javax.swing.JColorChooser;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JColorChooser;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
-    public class ColorChooserButton extends JButton {
+public class ColorChooserButton extends JButton {
 
-        private Color current;
-        private ImageFilters imgFilters;
+    private Color current;
+    private ImageFilters imgFilters;
 
-        public ColorChooserButton(Color c, ImageFilters imgFilters) {
-            this.imgFilters = imgFilters;
-            setSelectedColor(c); 
+    public ColorChooserButton(Color c, ImageFilters imgFilters) {
+        this.imgFilters = imgFilters;
+        setSelectedColor(c); 
 
-            addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent arg0) {
-                    DemoColorChooser dcc = new DemoColorChooser(imgFilters);
-                    Color newColor = dcc.chooser.getColor();
-                    setSelectedColor(newColor);
+        addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                DemoColorChooser dcc = new DemoColorChooser(imgFilters);
+                Color newColor = dcc.chooser.getColor();
+                if (imgFilters.selectedPolygon != null)
+                {
+                    newColor = imgFilters.selectedPolygon.color;
                 }
-            });
-            
-        }
+                setSelectedColor(newColor);
+                imgFilters.colorizePolygon(newColor);
+                imgFilters.repaint();
+            }
+        });
 
-        public Color getSelectedColor() {
-            return current;
-        }
+    }
 
-        public void setSelectedColor(Color newColor) {
-            setSelectedColor(newColor, true);
-        }
+    public Color getSelectedColor() {
+        return current;
+    }
 
-        public void setSelectedColor(Color newColor, boolean notify) {
+    public void setSelectedColor(Color newColor) {
+        setSelectedColor(newColor, true);
+    }
 
-            if (newColor == null) return;
+    public void setSelectedColor(Color newColor, boolean notify) {
 
-            current = newColor;
-            setIcon(createIcon(current, 16, 16));
-            repaint();
+        if (newColor == null) return;
 
-            if (notify) {
-                // Notify everybody that may be interested.
-                for (ColorChangedListener l : listeners) {
-                    l.colorChanged(newColor);
-                }
+        current = newColor;
+        setIcon(createIcon(current, 16, 16));
+        
+        repaint();
+
+        if (notify) {
+            // Notify everybody that may be interested.
+            for (ColorChangedListener l : listeners) {
+                l.colorChanged(newColor);
             }
         }
-
-        public static interface ColorChangedListener {
-            public void colorChanged(Color newColor);
-        }
-
-        private List<ColorChangedListener> listeners = new ArrayList<ColorChangedListener>();
-
-        public void addColorChangedListener(ColorChangedListener toAdd) {
-            listeners.add(toAdd);
-        }
-
-        public static  ImageIcon createIcon(Color main, int width, int height) {
-            BufferedImage image = new BufferedImage(width, height, java.awt.image.BufferedImage.TYPE_INT_RGB);
-            Graphics2D graphics = image.createGraphics();
-            graphics.setColor(main);
-            graphics.fillRect(0, 0, width, height);
-            graphics.setXORMode(Color.DARK_GRAY);
-            graphics.drawRect(0, 0, width-1, height-1);
-            image.flush();
-            ImageIcon icon = new ImageIcon(image);
-            return icon;
-        }
     }
+
+    public static interface ColorChangedListener {
+        public void colorChanged(Color newColor);
+    }
+
+    private List<ColorChangedListener> listeners = new ArrayList<ColorChangedListener>();
+
+    public void addColorChangedListener(ColorChangedListener toAdd) {
+        listeners.add(toAdd);
+    }
+
+    public static  ImageIcon createIcon(Color main, int width, int height) {
+        BufferedImage image = new BufferedImage(width, height, java.awt.image.BufferedImage.TYPE_INT_RGB);
+        Graphics2D graphics = image.createGraphics();
+        graphics.setColor(main);
+        graphics.fillRect(0, 0, width, height);
+        graphics.setXORMode(Color.DARK_GRAY);
+        graphics.drawRect(0, 0, width-1, height-1);
+        image.flush();
+        ImageIcon icon = new ImageIcon(image);
+        return icon;
+    }
+}
