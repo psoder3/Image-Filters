5fa34a6ac43f545413b7d695bd51ea54f7255709
psoder3 <paulsoderquist3@gmail.com>
2018-05-07 18:24:50 -0600

    Removed unnecessary panels in JColorChooser

--- src/imagefilters/DemoColorChooser.java
+++ src/imagefilters/DemoColorChooser.java
@@ -1,10 +1,16 @@
 package imagefilters;
 
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JColorChooser;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.colorchooser.AbstractColorChooserPanel;
+import javax.swing.colorchooser.ColorSelectionModel;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
@@ -15,7 +21,27 @@ public class DemoColorChooser {
     {
         this.imageFilters = imageFilters;
         chooser = new JColorChooser();
-        chooser.setColor(imageFilters.selectedPolygon.color);
+        AbstractColorChooserPanel[] oldPanels = chooser.getChooserPanels();
+        for (AbstractColorChooserPanel p: oldPanels) {
+            String display_name = p.getDisplayName();
+            if (display_name.equals("Swatches")) 
+            {
+                chooser.removeChooserPanel(p);
+            } 
+            else if (display_name.equals("HSL")) 
+            {
+                chooser.removeChooserPanel(p);
+            } 
+            else if (display_name.equals("CMYK")) 
+            {
+                chooser.removeChooserPanel(p);
+            }
+        }
+        chooser.setPreviewPanel(new MyPreviewPane(chooser));
+        if (imageFilters.selectedPolygon != null)
+        {
+            chooser.setColor(imageFilters.selectedPolygon.color);
+        }
         chooser.getSelectionModel().addChangeListener(new ChangeListener() {
             @Override
             public void stateChanged(ChangeEvent arg0) {
@@ -35,7 +61,10 @@ public class DemoColorChooser {
             public void actionPerformed(ActionEvent evt) {
                 
                 System.out.println("Clicked OK");
-                imageFilters.selectedPolygon.color = chooser.getColor();
+                if (imageFilters.selectedPolygon != null)
+                {
+                    imageFilters.selectedPolygon.color = chooser.getColor();
+                }
             }
         };
 
@@ -52,10 +81,32 @@ public class DemoColorChooser {
             }
         };
         
+        
         JDialog dialog = JColorChooser.createDialog(null, "Color Chooser",
                 true, chooser, okListener, cancelListener);
+
+        dialog.setLocation(0, 0);
         dialog.setVisible(true);
     }
     
     
+}
+
+class MyPreviewPane extends JLabel{
+  Color curColor;
+  public MyPreviewPane(JColorChooser chooser) {
+    curColor = chooser.getColor();
+    ColorSelectionModel model = chooser.getSelectionModel();
+    model.addChangeListener(new ChangeListener() {
+      public void stateChanged(ChangeEvent evt) {
+        ColorSelectionModel model = (ColorSelectionModel) evt.getSource();
+        curColor = model.getSelectedColor();
+      }
+    });
+    setPreferredSize(new Dimension(50, 50));
+  }
+  public void paint(Graphics g) {
+    g.setColor(curColor);
+    g.fillRect(0, 0, getWidth() - 1, getHeight() - 1);
+  }
 }
